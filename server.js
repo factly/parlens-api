46f51e607496aaa36a3cd465f8827bdac830a942
Ishank Saxena <ish.sxn@gmail.com>
2019-04-23 02:22:02 +0800

    Member APIs Activated

--- server.js
+++ server.js
@@ -15,18 +15,28 @@ app.use(express.static('public'));
 
 // init sqlite db
 var fs = require('fs');
-var ls_with_ministers = fs.readFileSync('./ls_16_member_list.json');
-var data = JSON.parse(ls_with_ministers);
+var ls_questions = fs.readFileSync('./ls_questions_with_ministers.json');
+var ls_members = fs.readFileSync('./ls_16_member_list.json');
+var rs_members = fs.readFileSync('./rs_member_list.json');
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-app.get('/lok_sabha', function(request, response) {
-  
+app.get('/lok_sabha_questions', function(request, response) {
+  var data = JSON.parse(ls_questions);
   response.send(JSON.stringify(data));
 });
 
+app.get('/lok_sabha_members', function(request, response) {
+  var data = JSON.parse(ls_members);
+  response.send(JSON.stringify(data));
+});
+
+app.get('/rajya_sabha_members', function(request, response) {
+  var data = JSON.parse(rs_members);
+  response.send(JSON.stringify(data));
+});
 // listen for requests :)
 var listener = app.listen(9000, function() {
   console.log('Your app is listening on port ' + listener.address().port);

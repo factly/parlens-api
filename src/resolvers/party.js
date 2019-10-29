107c61ec9db3d5e8b90879e8ee478b2e350a1ac9
Monark <monark@factly.in>
2019-10-29 18:15:12 +0530

    db reuse added

--- src/resolvers/party.js
+++ src/resolvers/party.js
@@ -1,13 +1,15 @@
-//import parties from '../../models/parties';
 import {MongoClient} from 'mongodb'
+
+//import db from './../utils/db';
+
 module.exports = {
 	async index() {
 		let client = await MongoClient.connect('mongodb+srv://dbroot:g7gwA4vdlPmwJdV5@cluster0-z1nlv.mongodb.net/test?retryWrites=true&w=majority', {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
 		})
-		let db = await client.db()
-		const parties = await db.collection('parties').find({}).toArray()
+		
+		const parties = await client.db().collection('parties').find({}).toArray()
 		return parties
 	},
 };

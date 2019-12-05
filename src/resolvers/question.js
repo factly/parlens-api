12c91d9b41f8bbe7826c78d8b6c744e07faa4aee
Monark <monark@factly.in>
2019-12-05 15:00:04 +0530

    some relief

--- src/resolvers/question.js
+++ src/resolvers/question.js
@@ -8,30 +8,33 @@ export async function index(
     }
 ) {
     let filter = {};
+    let nestedFilter = {}
     if (q) filter.subject = { $regex: q, $options: 'i' };
     if (house) filter.house = house;
     if (type) filter.type = type;
     if (ministry && ministry.length > 0) filter.ministry = { $in: ministry };
-    if (questionBy && questionBy.length > 0) filter['questionBy.MID'] = { $in: questionBy };
-    if (gender) filter['questionBy.gender'] = gender;
-    if (dob) filter['questionBy.dob'] = dob;
-    if (marital_status && marital_status.length > 0) filter['questionBy.marital_status'] = { $in: marital_status };
-    if (education && education.length > 0) filter['questionBy.education'] = { $in: education };
-    if (profession && profession.length > 0) filter['questionBy.profession'] = { $in: profession };
-    if (expertise && expertise.length > 0) filter['questionBy.expertise'] = { $in: expertise };
-    if (sons && sons.length > 0) filter['questionBy.sons'] = { $in: sons };
-    if (daughters && daughters.length > 0) filter['questionBy.daughters'] = { $in: daughters };
-    if (term) filter['questionBy.terms'] = { $size: term };
-    if (party && party.length > 0) filter['questionBy.terms.party'] = { $in: party };
-    if (constituency && constituency.length > 0) filter['questionBy.terms.constituency'] = { $in: constituency };
-    
+    if (questionBy && questionBy.length > 0) filter.questionBy = { $in: questionBy };
+    if (gender) nestedFilter['questionBy.gender'] = gender;
+    if (dob) nestedFilter['questionBy.dob'] = dob;
+    if (marital_status && marital_status.length > 0) nestedFilter['questionBy.marital_status'] = { $in: marital_status };
+    if (education && education.length > 0) nestedFilter['questionBy.education'] = { $in: education };
+    if (profession && profession.length > 0) nestedFilter['questionBy.profession'] = { $in: profession };
+    if (expertise && expertise.length > 0) nestedFilter['questionBy.expertise'] = { $in: expertise };
+    if (sons && sons.length > 0) nestedFilter['questionBy.sons'] = { $in: sons };
+    if (daughters && daughters.length > 0) nestedFilter['questionBy.daughters'] = { $in: daughters };
+    if (term) nestedFilter['questionBy.terms'] = { $size: term };
+    if (party && party.length > 0) nestedFilter['questionBy.terms.party'] = { $in: party };
+    if (constituency && constituency.length > 0) nestedFilter['questionBy.terms.constituency'] = { $in: constituency };
+
     const pageLimit = limit && limit > 0 && limit < 20 ? limit : 10;
     const pageSkip = page ? (page - 1) * pageLimit : 0;
 
-    logger('info', 'fetching questions for query ' + JSON.stringify(filter));
-
-    console.time('que')
+    logger('info', 'fetching questions for query ' + JSON.stringify(filter) + JSON.stringify(nestedFilter));
+    console.time("que")
     const res = await db.collection(config.db.questions).aggregate([
+        {
+            $match: filter
+        },
         {
             $unwind: {
                 'path': '$questionBy'
@@ -46,7 +49,26 @@ export async function index(
             }
         },
         {
-            $match: filter
+            $unwind: {
+                'path': '$questionBy'
+            }
+        },
+        {
+            $group: {
+                '_id': '$_id',
+                'questionBy': { '$push': '$questionBy' },
+                'QID': { '$first': '$QID' },
+                'subject': { '$first': '$subject' },
+                'type': { '$first': '$type' },
+                'question': { '$first': '$question' },
+                'answer': { '$first': '$answer' },
+                'house': { '$first': '$house' },
+                'ministry': { '$first': '$ministry' },
+                'date': { '$first': '$date' },
+            }
+        },
+        {
+            $match: nestedFilter
         },
         {
             $sort: { QID: -1 }
@@ -57,8 +79,8 @@ export async function index(
         {
             $limit: pageLimit  
         },
-    ]).toArray();
-    console.timeEnd('que')
+    ]).toArray()
+    console.timeEnd("que")
     return res;
 }
 
@@ -69,61 +91,35 @@ export async function single({ db, logger, config }, { id }) {
 
     const result = await db.collection(config.db.questions).aggregate([
         {
+            $unwind: {
+                'path': '$questionBy'
+            }
+        },
+        {
             $lookup: {
                 from: config.db.members,
-                let: { questionBy: '$questionBy' },
-                pipeline: [
-                    {
-                        $match: {
-                            $expr: { $in: ['$MID', '$$questionBy'] }
-                        }
-                    },
-                    {
-                        $unwind: { path: '$terms', preserveNullAndEmptyArrays: true }
-                    },
-                    {
-                        $lookup: {
-                            'from': config.db.parties,
-                            'localField': 'terms.party',
-                            'foreignField': 'PID',
-                            'as': 'terms.party'
-                        }
-                    },
-                    {
-                        $lookup: {
-                            'from': config.db.constituencies,
-                            'localField': 'terms.constituency',
-                            'foreignField': 'CID',
-                            'as': 'terms.constituency'
-                        }
-                    },
-                    {
-                        $unwind: '$terms.constituency'
-                    },
-                    {
-                        $unwind: '$terms.party'
-                    },
-                    {
-                        $group: {
-                            '_id': '$_id',
-                            'terms': { '$push': '$terms' },
-                            'gender': { '$first': '$gender' },
-                            'MID': { '$first': '$MID' },
-                            'name': { '$first': '$name' },
-                            'dob': { '$first': '$dob' },
-                            'birth_place': { '$first': '$birth_place' },
-                            'marital_status': { '$first': '$marital_status' },
-                            'sons': { '$first': '$sons' },
-                            'daughters': { '$first': '$daughters' },
-                            'email': { '$first': '$email' },
-                            'phone': { '$first': '$phone' },
-                            'education': { '$first': '$education' },
-                            'expertise': { '$first': '$expertise' },
-                            'profession': { '$first': '$profession' }
-                        }
-                    },
-                ],
+                localField: 'questionBy',
+                foreignField: 'MID',
                 as: 'questionBy'
+            }
+        },
+        {
+            $unwind: {
+                'path': '$questionBy'
+            }
+        },
+        {
+            $group: {
+                '_id': '$_id',
+                'questionBy': { '$push': '$questionBy' },
+                'QID': { '$first': '$QID' },
+                'subject': { '$first': '$subject' },
+                'type': { '$first': '$type' },
+                'question': { '$first': '$question' },
+                'answer': { '$first': '$answer' },
+                'house': { '$first': '$house' },
+                'ministry': { '$first': '$ministry' },
+                'date': { '$first': '$date' },
             }
         },
         {
